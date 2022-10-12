5a32f9f49301449a4748d290b163b1a72b5b0c61
BastianGH <bastianpussey16@gmail.com>
2022-10-12 02:47:45 +0200

    ajout

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,60 @@
 <template>
-  <Header_comp/>
-  <HomePage />
-</template>
-
-<script>
-import Header_comp from "./components/Header.vue";
-import HomePage from "@/components/Home-page";
-
-export default {
-  name: "App",
+  <Header_comp />
+  <router-view />
+ <Footer_comp />
+ </template>
+  
+ <script>
+ import Header_comp from './components/Header.vue';
+ import Footer_comp from './components/Footer.vue';
+  
+ export default {
+  name: 'App',
   components: {
-    Header_comp,
-    HomePage,
-  },
-};
-</script>
-
-<style>
-* {
+    Footer_comp,
+    Header_comp
+  }
+ }
+ </script>
+  
+ <style>
+ * {
   box-sizing: content-box;
-  font-family: "Poppins", sans-serif;
-}
-
-li {
+  font-family: 'Poppins', sans-serif;
+ }
+ li {
   list-style: none;
-}
-
-a {
+ }
+ a {
   text-decoration: none;
-}
-</style>
+ }
+ .bgc-dark-blue {
+  background-color: #14142C;
+  color: white;
+ }
+ .bgc-grey {
+  background-color: #F5F5F5;
+ }
+ section {
+  height: 1080px;
+ }
+ .lavender-text {
+  color: #9EAAFF;
+ }
+ .lavender-box {
+  color: white;
+  background-color: #9EAAFF;
+ }
+ .lavender-button {
+  color: #9EAAFF;
+  border: solid 3px #9EAAFF;
+  background-color: #0000;
+  font-weight: 500;
+  font-size: 137%;
+  line-height: 154%;
+  border-radius: 50px;
+  padding: 1% 4%;
+ }
+  
+ </style>
+ 
