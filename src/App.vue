f99bec856f76a95e7b7cccd7ba00539265c18391
BastianGH <bastianpussey16@gmail.com>
2022-10-19 18:04:22 +0200

    suppression de lignes inutiles postmerge

--- src/App.vue
+++ src/App.vue
@@ -60,67 +60,18 @@ body {
 .lavender-text {
   color: #9EAAFF;
 }
-.lavender-box {
-  color: white;
-  background:linear-gradient(  .25turn , #9EAAFF 100%,white 100% );
-  transform: translateX(5);
-  border: 50px linear #9EAAFF;
-}
-.lavender-box:hover {
-  color: #9EAAFF;
-  transition: color 1ms, border 1ms;
-  animation: button-opening 4000ms;
-}
-.lavender-button {
-  color: #9EAAFF;
-  border: solid 3px #9EAAFF;
-  background-color: #0000;
-  font-weight: 500;
-  font-size: 137%;
-  line-height: 154%;
-  border-radius: 50px;
-  padding: 1% 4%;
-}
 .bgc-grey {
   background-color: #F5F5F5;
 }
 .lavender-text {
   color: #9EAAFF;
 }
-.lavender-box {
-  color: white;
-  background-color: #9EAAFF;
-}
-.lavender-button {
-  color: #9EAAFF;
-  border: solid 3px #9EAAFF;
-  background-color: #0000;
-  font-weight: 500;
-  font-size: 137%;
-  line-height: 154%;
-  border-radius: 50px;
-  padding: 1% 4%;
-}
 .bgc-grey {
   background-color: #F5F5F5;
 }
 .lavender-text {
   color: #9EAAFF;
-}
-.lavender-box {
-  color: white;
-  background-color: #9EAAFF;
-}
-.lavender-button {
-  color: #9EAAFF;
-  border: solid 3px #9EAAFF;
-  background-color: #0000;
-  font-weight: 500;
-  font-size: 137%;
-  line-height: 154%;
-  border-radius: 50px;
-  padding: 1% 4%;
-}
+ }
 section {
   height: 1080px;
 }
