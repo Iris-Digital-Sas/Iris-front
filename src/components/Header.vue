28dab937a6a241bfde98fada20419b3b7d9a2087
BastianGH <bastianpussey16@gmail.com>
2022-11-21 18:44:40 +0100

    MàJ du menu burger

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,28 +10,24 @@
         <li><router-link to="/contact">CONTACT</router-link></li>
       </ul>
     </nav>
-    <div>
-      <Slide closeOnNavigation="true" right id="slide">
-        <ul class="slide-nav-list">
-          <li><router-link to="/solutions">NOS SOLUTIONS</router-link></li>
-          <li><router-link to="/team">L'EQUIPE</router-link></li>
-          <li><router-link to="/achievements">REALISATIONS</router-link></li>
-          <li><router-link to="/prices">NOS TARIFS</router-link></li>
-          <li><router-link to="/contact">CONTACT</router-link></li>
-        </ul>
-    </Slide>
-    </div>    
+    <input id="menu-toggle" type="checkbox" />
+    <label class='menu-button-container' for="menu-toggle">
+    <div class='menu-button'></div>
+  </label>
+    <ul class="menu">
+      <li><router-link to="/solutions">NOS SOLUTIONS</router-link></li>
+        <li><router-link to="/team">L'EQUIPE</router-link></li>
+        <li><router-link to="/achievements">REALISATIONS</router-link></li>
+        <li><router-link to="/prices">NOS TARIFS</router-link></li>
+        <li><router-link to="/contact">CONTACT</router-link></li>
+    </ul>   
   </header>
 </template>
 
 <script>
-import { Slide } from 'vue3-burger-menu'  
 
 export default {
-  name: "Header_comp",
-  components: {
-        Slide,
-  }
+  name: "Header_comp"
 }
 </script>
 
@@ -64,44 +60,110 @@ export default {
 .header-nav-list a:hover {
   color: #9EAAFF;
 }
-#slide{
+.menu {
+  display: flex;
+  flex-direction: row;
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+.menu > li {
+  margin: 0 1rem;
+  overflow: hidden;
+}
+
+.menu-button-container {
+  display: none;
+  height: 100%;
+  width: 30px;
+  cursor: pointer;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+#menu-toggle {
   display: none;
 }
-.bm-menu {
-  background-color: rgb(20, 20, 44);
-  padding-top: 90px;
-  height: 35%;
+
+.menu-button,
+.menu-button::before,
+.menu-button::after {
+  display: block;
+  background-color: #fff;
+  position: absolute;
+  height: 4px;
+  width: 30px;
+  transition: transform 400ms cubic-bezier(0.23, 1, 0.32, 1);
+  border-radius: 2px;
 }
-.bm-item-list {
-  height: 100%;
-  padding: 0;
+
+.menu-button::before {
+  content: '';
+  margin-top: -8px;
 }
-.bm-burger-button {
-  left: 350px;
-  top: 30px;
+
+.menu-button::after {
+  content: '';
+  margin-top: 8px;
 }
-.bm-burger-button > * {
-  background-color: white;
+
+#menu-toggle:checked + .menu-button-container .menu-button::before {
+  margin-top: 0px;
+  transform: rotate(405deg);
 }
-.bm-cross-button, .bm-burger-button {
-  top: 30px;
-  left: 75%;
+
+#menu-toggle:checked + .menu-button-container .menu-button {
+  background: rgba(255, 255, 255, 0);
 }
-.slide-nav-list {
-  padding: 0;
-  margin: 0;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
+
+#menu-toggle:checked + .menu-button-container .menu-button::after {
+  margin-top: 0px;
+  transform: rotate(-405deg);
 }
-.slide-nav-list a {
-  font-weight: 400;
-  font-size: 18px;
-  color: white;
-}
-.slide-nav-list a:hover {
-  color: #9EAAFF;
+
+@media (max-width: 700px) {
+  .menu-button-container {
+    display: flex;
+  }
+  .menu {
+    position: absolute;
+    top: 0;
+    margin-top: 77px;
+    left: 0;
+    flex-direction: column;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+  }
+  #menu-toggle ~ .menu li {
+    height: 0;
+    margin: 0;
+    padding: 0;
+    border: 0;
+    transition: height 400ms cubic-bezier(0.23, 1, 0.32, 1);
+  }
+  #menu-toggle:checked ~ .menu li {
+    border: 1px solid #333;
+    height: 2.5em;
+    padding: 0.5em;
+    transition: height 400ms cubic-bezier(0.23, 1, 0.32, 1);
+  }
+  .menu > li {
+    display: flex;
+    justify-content: center;
+    margin: 0;
+    padding: 0.5em 0;
+    width: 100%;
+    color: white;
+    background-color: rgba(20, 20, 44, 1);;
+  }
+  .menu > li a{
+    color: white;
+  }
+  .menu > li:not(:last-child) {
+    border-bottom: 1px solid #444;
+  }
 }
 
 @media screen and ( max-width: 600px) {
